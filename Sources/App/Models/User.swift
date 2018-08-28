ea84775da7fffeee76f82e30a6953a69e64526ed
Paul Tiarks <paultiarks@gmail.com>
2018-08-28 08:49:50 -0500

    Add auth and csrf protection

--- Sources/App/Models/User.swift
+++ Sources/App/Models/User.swift
@@ -89,3 +89,6 @@ struct AdminUser: Migration {
         return .done(on: connection)
     }
 }
+
+extension User: PasswordAuthenticatable {}
+extension User: SessionAuthenticatable {}
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -19,6 +19,7 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
     middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
+    middlewares.use(SessionsMiddleware.self)
     services.register(middlewares)
 
     var databases = DatabasesConfig()
@@ -60,5 +61,6 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     services.register(commandConfig)
 
     config.prefer(LeafRenderer.self, for: ViewRenderer.self)
+    config.prefer(MemoryKeyedCache.self, for: KeyedCache.self)
 
 }
